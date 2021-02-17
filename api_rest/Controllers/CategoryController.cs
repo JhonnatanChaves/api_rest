75b79dc6b892b11f66c66c35d43394b1e644218f
Jhonnatan Chaves <chavesjhonnatan@gmail.com>
2021-02-17 15:30:38 -0300

    :construction: Criando uma rota de controle para a lista de categorias

---
+++ api_rest/Controllers/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api_rest.Domains.Services;
+using Microsoft.AspNetCore.Mvc;
+using Supermarket.API.Domains.Models;
+
+namespace api_rest.Controllers
+{
+    [Route("api_rest/[controller]")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            var categories = await _categoryService.ListAsync();
+            return categories;
+        }
+    }
+}
