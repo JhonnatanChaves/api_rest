920e116a56850ad48d444a66ce788ca7d07f4657
Jhonnatan Chaves <chavesjhonnatan@gmail.com>
2021-02-17 13:49:24 -0300

    :rocket: Implementando a Camada de Domínio

---
+++ api_rest/Domains/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Supermarket.API.Domains.Models
+{
+    public class Category
+    {
+
+
+        public int id { get; set; }
+
+        public string Name { get; set; }
+
+        //Essa propriedade de produtos vai permitir que categorias e produtos 
+        public IList<Product> Products { get; set; } = new List<Product>();
+
+
+
+
+
+    }
+}
---
+++ api_rest/Domains/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api_rest.Domains.Helpers;
+using Supermarket.API.Domains.Models;
+
+namespace api_rest.Domains.Models
+{
+    public class Product
+    {
+
+		public int id { get; set; };
+
+		public string Name { get; set; }
+
+		public short QuatityInPackage { get; set; }
+
+		//enumera as unidades de medida 
+		public EUnitOfMeasurement UnitOfMeasurement { get; set; }
+
+
+		//permite que um produto possua apenas uma categoria
+		public int CategoryId { get; set; }
+		public Category Category { get; set; }
+
+	}
+}
