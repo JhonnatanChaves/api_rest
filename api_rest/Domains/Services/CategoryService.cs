f0c303844963358f5c7fe79a58361cac942f8f7b
Jhonnatan Chaves <chavesjhonnatan@gmail.com>
2021-02-17 15:42:38 -0300

    :construction_worker: Implementando a Camada de Serviços

---
+++ api_rest/Domains/Services/CategoryService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Supermarket.API.Domains.Models;
+
+namespace api_rest.Domains.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        public Task<IEnumerable<Category>> ListAsync()
+        {
+            return null;
+        }
+    }
+}
---
+++ api_rest/Domains/Services/ICategoryService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Supermarket.API.Domains.Models;
+
+namespace api_rest.Domains.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> ListAsync();
+
+    }
+}
